bfc830b2691ea5c63c8fc4c31c74796f6bd84d2b
Moshe Swed <mswed@beapot.com>
2024-06-09 01:29:25 -0400

    stack

--- stack.js
+++ stack.js
@@ -20,6 +20,16 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    // The end of the stack is actually the first node, since this is a linkedList
+    if (this.isEmpty()) {
+      this.first = new Node(val);
+      this.last = this.first
+    } else {
+      const newFirst = new Node(val);
+      newFirst.next = this.first;
+      this.first = newFirst;
+    }
+    this.size ++;
 
   }
 
@@ -27,18 +37,30 @@ class Stack {
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.isEmpty()) {
+      throw new Error('The stack is empty!')
+    } else {
+      const originalFirst = this.first;
+      this.first = originalFirst.next;
+      originalFirst.next = null;
+      this.size--;
 
+      return originalFirst.val;
+
+    }
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
+    return this.first.val;
 
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
+    return this.size === 0;
 
   }
 }
